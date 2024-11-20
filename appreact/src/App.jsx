93d9458e943ddef324916cd3887ec16c309eff8d
gustavordd <rodriguesgustavo319@gmail.com>
2024-11-20 02:27:26 -0300

    first commit

---
+++ appreact/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+    return (
+        <div>
+            <hr />
+            <h1> <center>Trabalhando com componentes em React </center></h1>
+            <hr />
+        </div>
+    )
+ 
+}
+
+export default App
